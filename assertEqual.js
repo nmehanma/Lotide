4c9a24b6fac8f6cc4a7c7204901c1ff89372d412
Najeam <nmehanma@gmal.com>
2019-09-16 20:11:27 +0000

    edited

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual +  "=== " + expected);
+  } else {
+    console.log("Assertion Failed: " +  actual + "!== " + expected);
+  }
+
+
+};
+
+//Test Code
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(2, 1);
