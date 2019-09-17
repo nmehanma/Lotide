d7c45a740e7d9e4042be1b0d2077accafb1c14d2
Najeam <nmehanma@gmal.com>
2019-09-17 03:45:16 +0000

    reverse

---
+++ reverse.js
@@ -0,0 +1,13 @@
+function backwards(str) {
+  var newstring = "";
+
+  for(var i = str.length - 1; i >= 0; i--) {
+    newstring += str.charAt(i);
+  }
+
+  return newstring;
+}
+
+
+console.log(backwards("Hello Goodbye"))
+
