f8de9abc9ee08a6a971e10c443ae35aced7c9213
Najeam <nmehanma@gmal.com>
2019-09-17 02:37:38 +0000

    tail

--- tail.js
+++ tail.js
@@ -1,26 +1,33 @@
+const Passed = 'Assertion Passed';
+const Failed = 'Assertion Failed';
+const equal = "===";
+const notequal = "!==";
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual +  "=== " + expected);
+    console.log(`${Passed}`, actual,  `${equal}`, expected);
   } else {
-    console.log("Assertion Failed: " +  actual + "!== " + expected);
+    console.log(`${Failed}`, actual, `${notequal}`, expected);
   }
-
 
 };
 
 //Test Code
 
 
-let tail = function(array){
+let tail = function(array) {
   return array.slice(1);
 
-}
+};
   
 
-  console.log(tail([5,6,7]))
 
 
+console.log(tail([5,6,7]));
 
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
 // assertEqual("Lighthouse Labs", "Bootcamp");
 // assertEqual(1, 1);
