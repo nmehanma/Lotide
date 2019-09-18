781015ddd346aa6d33e4b64b7622b8ebd2c98212
Najeam <nmehanma@gmal.com>
2019-09-18 17:33:07 +0000

    edited

--- countOnly.js
+++ countOnly.js
@@ -18,17 +18,20 @@ const countOnly = function(allItems, itemsToCount) {
   const results = {}
 
   for (const item of allItems) {
+    console.log(item);
 
-  
-  
-  if (itemsToCount[item]) {
     if (results[item]) {
       results[item] += 1;
     } else {
       results[item] = 1;
     }
-  }
-    console.log(item);
+
+    
+
+  
+  
+  
+
   }
 
   return results;
