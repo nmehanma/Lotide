fc89265cb54ae378f6c9426dc7c3c645051e8160
Najeam <nmehanma@gmal.com>
2019-09-20 03:28:49 +0000

    middle function

--- middle.js
+++ middle.js
@@ -1,3 +1,104 @@
+// Test Assertion Functions
+
+const Passed = 'Assertion Passed'
+const Failed = 'Assertion Failed'
+const equal = "==="
+const notequal = "!=="
+
+
+// eqArrays function
+
+const eqArrays = function(array1, array2) {
+
+  if( array1.length !== array2.length) {
+    return false
+  }
+
+  for(let i = 0; i < array1.length; i++) {
+
+    let a = array1[i]
+    let b = array2[i]
+
+    if (a !== b) {
+      return false
+    }
+  } return true
+  
+}
+
+
+// assertArraysEqual function
+
+
+const assertArraysEqual = function (actual, expected) {
+
+  if (eqArrays(actual, expected)) {
+    console.log(`${Passed}`, actual,  `${equal}`, expected);
+  } else {
+    console.log(`${Failed}`, actual, `${notequal}`, expected);
+  }
+ 
+  
+}
+
+
+
+// Actual Function
+
+const middle = function(arr) {
+
+  let x = []
+  if( arr.length === 1 || arr.length === 2) {
+    x = undefined;
+
+  } else if(arr.length % 2 !== 0) {
+      x = arr.splice(Math.floor(arr.length / 2), 1);
+      // console.log("true")
+      
+      
+    } else {
+      x = arr.splice(Math.floor(arr.length / 2) - 1, 2)
+    }
+  
+  return x
+  
+}
+
+
+
+
+// middle([1]) // => []
+// middle([1,2]) //=> []
+
+
+// const result2 = middle([1]) // => undefined
+
+const result1 = middle([1,2,3]) // => [2]
+
+const result2 = middle([1,2,3,4,5]) // => [3]
+
+const result3 = middle ([1,2,3,4]) // => [2,3]
+
+const result4 = middle ([1,2,3,4,5,6,])
+
+
+assertArraysEqual(result1.length, result2.length)
+
+assertArraysEqual(result3.length, result4.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const Passed = 'Assertion Passed'
 // const Failed = 'Assertion Failed'
 // const equal = "==="
@@ -36,32 +137,32 @@
 
 // Actual Function
 
-const middle = function(array) {
+// const middle = function(array) {
   
-  let x =[]
+//   let x =[]
 
-  for (let i = 0; i <array.length; i++){
+//   for (let i = 0; i <array.length; i++){
 
-   if (array.length === 1 || array.length === 2) {
+//    if (array.length === 1 || array.length === 2) {
      
-    let x  = []
+//     let x  = []
 
-   } else if(array.length % 2 !== 0) {
+//    } else if(array.length % 2 !== 0) {
       
-      let x = array[Math.ceil(array.length /2)]
+//       let x = array[Math.ceil(array.length /2)]
 
-    } else if(array.length % 2 === 0) {
+//     } else if(array.length % 2 === 0) {
 
-      let x = array[array.length/2] + [Math.ceil(array.length/2)]
-    }
+//       let x = array[array.length/2] + [Math.ceil(array.length/2)]
+//     }
 
-    return (x)
+//     return (x)
 
-  }
+//   }
 
   
-}
+// }
 
-console.log(x)
+// console.log(x)
   
-middle([1,2,3,4])
+// middle([1,2,3,4])
