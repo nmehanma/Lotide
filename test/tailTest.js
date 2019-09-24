b90ee81148cd37d252d6b2cc51cf9710e3f1663c
Najeam <nmehanma@gmal.com>
2019-09-24 00:23:39 +0000

    editing

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,38 +2,11 @@
 
 const assert = require('chai').assert;
 
-const middle = require('../tail');
+const tail = require('../tail');
 
 
 
 // const assertEqual = require('../assertEqual')
-
-
-describe("#middle", () => {
-  assert.strictEqual(middle([1,2,3]), 2);
-})
-
-// describe("#head", () => {
-//   it("returns 1 for [1,2,3]", () => {
-//     assert.strictEqual(head([1,2,3]),1);
-
-//   });
-
-//   it("returns '5' for ['5']", () => {
-//     assert.strictEqual(head(['5']),'5')
-//   });
-
-//   it("does not return '2' for [1,2,3]", () => {
-//     assert.notStrictEqual(head(['2']), [1,2,3])
-//   })
-
-// });
-
-
-
-
-
-
 
 describe('#tail', () => {
   it('returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]',() => {
